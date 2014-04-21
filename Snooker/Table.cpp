5c01d93959350cb8eba8b66fad75faeb72c05fd9
Zhu Jinchang <764109407@qq.com>
2014-04-21 22:14:38 +0800

    Add some standard data and the rule

---
+++ Snooker/Table.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "Table.h"
+
+const double Table::STD_WIDTH = 3.569;
+const double Table::STD_HEIGHT = 1.778;
+const double Table::STD_HOLE_SIZE = 0.085;
+const double Table::STD_BLACK_BALL_POS = 0.324;
